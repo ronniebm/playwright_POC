3d712c290ad8ff443754bf106c05b8bb876ff5e5
ronniebm <ronnie.coding@gmail.com>
2022-11-05 12:44:29 -0500

    adding entry_page context handler, @then("is redirected to the entry page") added.

--- tests/step_defs/test_login_bdd.py
+++ tests/step_defs/test_login_bdd.py
@@ -3,6 +3,7 @@ from pytest_bdd import scenarios, given, when, then
 from playwright.sync_api import Page, BrowserContext
 from tests.pages.login import LoginPage
 from tests.pages.environment import EnvironmentPage
+from tests.pages.medi_invoices_entry import EntryPage
 
 import time
 
@@ -42,5 +43,12 @@ def step_impl(context: BrowserContext):
 @when('the user set client and service area')
 def step_impl(context: BrowserContext):
     env_page = context.current_page
-    env_page.set_client_and_service_area('MannKind Corporation', 'Medicaid')
+    entry_page = EntryPage(env_page.set_client_and_service_area('MannKind Corporation', 'Medicaid'))
+    context.current_page = entry_page
+
+
+@then('is redirected to the entry page')
+def step_impl(context: BrowserContext):
+    entry_page = context.current_page
+    entry_page.create_new_invoice()
     time.sleep(20)
